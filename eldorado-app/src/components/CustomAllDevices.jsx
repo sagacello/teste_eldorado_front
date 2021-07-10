7f55a03b2cd93e49d839b3cf62ff7f6815aa758c
marcelo ivan <sagacello@hotmail.com>
2021-07-10 20:29:28 -0300

    melhoria na pagina principal

--- eldorado-app/src/components/CustomAllDevices.jsx
+++ eldorado-app/src/components/CustomAllDevices.jsx
@@ -1,22 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Card } from 'semantic-ui-react';
+import { Button, Card, Divider } from 'semantic-ui-react';
 import CustomSubHeader from './CustomSubHeader';
 
-const CustomAllDevices = ({ index, item, deleteDevices }) => {
+const CustomAllDevices = ({ item, deleteDevices }) => {
   return (
-    <Card.Group>
-      <Card>
-        <Card.Content>
-          <Card.Header>
-            <CustomSubHeader
+    <Card.Group >
+      <Card style={{ padding: '10px', backgroundColor: 'rgb(230, 230, 230)' }} >
+        <Card.Content style={{ padding: '1px' }}>
+          <Card.Header >
+            <CustomSubHeader 
               message={`${item.category.name}`}
             ></CustomSubHeader>{' '}
+            <Divider />
           </Card.Header>
-          <Card.Meta>{item.color}</Card.Meta>
-          <Card.Description>{`partNumber ${item.partNumber}`}</Card.Description>
+          <Card.Meta style={{ padding: '4px' }}>{item.color}</Card.Meta>
+          <Card.Description style={{ padding: '4px' }}>{`partNumber ${item.partNumber}`}</Card.Description>
           <Card.Content extra>
-            <Button onClick={() => deleteDevices(item.id)} inverted color="red">
+          <Divider />
+            <Button style={{ padding: '10px' }} onClick={() => deleteDevices(item.id)} inverted color="red">
               Excluir
             </Button>
           </Card.Content>
@@ -27,7 +29,8 @@ const CustomAllDevices = ({ index, item, deleteDevices }) => {
 };
 
 CustomAllDevices.propTypes = {
-  index: PropTypes.number.isRequired,
+  item: PropTypes.object.isRequired,
+  deleteDevices: PropTypes.func.isRequired,
 };
 
 export default CustomAllDevices;
