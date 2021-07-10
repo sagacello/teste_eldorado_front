5329fec582babdc3cd46f4e1bda2dd4fb029fe80
marcelo ivan <sagacello@hotmail.com>
2021-07-10 14:34:25 -0300

    todas as categorias sao listadas no dropdown, e se uma for atualizada tambem é acrescentada

--- eldorado-app/src/components/CustomManagement.jsx
+++ eldorado-app/src/components/CustomManagement.jsx
@@ -1,16 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Form, Segment } from 'semantic-ui-react';
+import { Menu, Dropdown, Icon } from 'semantic-ui-react';
+import fetchCategories from '../services/ManagementService';
 
-const CustomManagement = ({
- 
-}) => {
+const CustomManagement = ({ createCatgory, createDevice }) => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const allCatgories = await fetchCategories();
+      setCategories(allCatgories);
+    };
+    fetch();
+  }, []);
+
   return (
     <div>
-     
+      <Menu attached="top">
+        <Dropdown item icon="play" simple>
+          <Dropdown.Menu>
+            <Dropdown.Item>
+              <Icon name="dropdown" />
+              <span className="text">Categories</span>
+              <Dropdown.Menu>
+                {!categories ? (
+                  <span className="text">Categories</span>
+                ) : (
+                  categories.map((category, index) => {
+                    return (
+                      <Dropdown.Item key={index}>{category.name}</Dropdown.Item>
+                    );
+                  })
+                )}
+              </Dropdown.Menu>
+            </Dropdown.Item>
+            <Dropdown.Item onClick={() => createCatgory()}>
+              Create category
+            </Dropdown.Item>
+            <Dropdown.Item onClick={() => createDevice()}>
+              Create device
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Menu.Menu position="right">
+          <div className="ui right aligned category search item">
+            <div className="ui transparent icon input">
+              <input
+                className="prompt"
+                type="text"
+                placeholder="Search devices..."
+              />
+              <i className="search link icon" />
+            </div>
+            <div className="results" />
+          </div>
+        </Menu.Menu>
+      </Menu>
     </div>
   );
 };
-
 
 export default CustomManagement;
